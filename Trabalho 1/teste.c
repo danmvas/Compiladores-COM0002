556bd86a57d2bad3b5baea7e2f3ca0abd3212fb8
Matheus Soppa Geremias <suppersoppa@gmail.com>
2021-06-11 19:46:23 -0300

    finalizado gramatica, iniciado flex

--- Trabalho 1/teste.c
+++ Trabalho 1/teste.c
@@ -7,8 +7,8 @@ int main()
 {
 	int numeros[TAM] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 	int i, aux, contador;
-	int include = 10;
-    printf("int true TRUE %d\n", include); // main  não é keyword
+	int function = 10;
+    printf("int true TRUE %d\n", function); // main  não é keyword
 	
 	return 0;
 }
